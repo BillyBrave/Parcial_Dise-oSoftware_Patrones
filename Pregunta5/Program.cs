9ab863710934f0c39ec740df3fb0a5b8d397e399
BillyBrave <billy.brave@gmail.com>
2017-10-14 11:01:46 -0500

    commit

---
+++ Pregunta5/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta5
+{
+    // La interface 'Product' 
+
+    public interface IFactory
+    {
+        void Registrar(String dato);
+    }
+
+    // La clase 'ConcreteProduct' 
+    public class FileLogger : IFactory 
+    {
+        public void Registrar(String dato)
+        {
+            Console.WriteLine("Se registro en File : " + dato );
+        }
+    }
+
+
+    //Una clase 'ConcreteProduct' 
+
+    public class BDLogger : IFactory
+    {
+        public void Registrar(String dato)
+        {
+            Console.WriteLine("Se registro en BD : " + dato);
+        }
+    }
+
+    //Una clase 'ConcreteProduct' 
+
+    public class NetworkLogger : IFactory
+    {
+        public void Registrar(String dato)
+        {
+            Console.WriteLine("Se registro en Network : " + dato);
+        }
+    }
+    //La clase Abstract
+
+    public abstract class LoggerFactory
+    {
+        public abstract IFactory GetTIPO(String tipo);
+
+    }
+
+
+    // Una clase 'ConcreteCreator' 
+
+    public class ConcreteTIPOFactory : LoggerFactory
+    {
+        public override IFactory GetTIPO(String tipo)
+        {
+            switch (tipo)
+            {
+                case "File":
+                    return new FileLogger();
+                case "BD":
+                    return new BDLogger();
+                case "NetWork":
+                    return new NetworkLogger();
+                default:
+                    throw new ApplicationException(string.Format("TIPO '{0}' no se puede crear", tipo));
+            }
+        }
+
+    }
+
+
+    // Demostracion de Patron Factory 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoggerFactory factory = new ConcreteTIPOFactory();
+
+            IFactory File = factory.GetTIPO("File");
+            File.Registrar(" Nombre Juan Perez DNI 07504899");
+
+            IFactory BD = factory.GetTIPO("BD");
+            BD.Registrar(" Nombre Juan Perez DNI 07504899");
+
+            IFactory Network = factory.GetTIPO("NetWork");
+            Network.Registrar(" Nombre Juan Perez DNI 07504899");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Pregunta6/Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta6
+{
+
+    // La interface 'AbstractFactory' 
+
+    interface ProductoFactory
+    {
+        Hotel GetHotel(string Hotel);
+        Airplane GetAirplane(string Airplane);
+    }
+
+    // La clase 'ConcreteFactory1' 
+
+    class HotelFactory : ProductoFactory
+    {
+        public Hotel GetHotel(string Servicio)
+        {
+            switch (Servicio)
+            {
+                case "tres":
+                    return new Hotel3Estrellas();
+                case "cinco":
+                    return new Hotel5Estrellas();
+                default:
+                    throw new ApplicationException(string.Format("Servicio '{0}' no puede ser brindado", Servicio));
+            }
+
+        }
+
+        public Airplane GetAirplane(string Servicio)
+        {
+            switch (Servicio)
+            {
+                case "ejecutivo":
+                    return new Ejecutivo();
+                case "turista":
+                    return new Turista();
+                default:
+                    throw new ApplicationException(string.Format("Servicio '{0}' no puede ser brindado", Servicio));
+            }
+
+        }
+    }
+
+    /// La clase 'ConcreteFactory2' 
+
+    class AirplaneFactory : ProductoFactory
+    {
+        public Hotel GetHotel(string Servicio)
+        {
+            switch (Servicio)
+            {
+                case "tres":
+                    return new Hotel3Estrellas();
+                case "cinco":
+                    return new Hotel5Estrellas();
+                default:
+                    throw new ApplicationException(string.Format("Servicio '{0}' no puede ser brindado", Servicio));
+            }
+
+        }
+
+        public Airplane GetAirplane(string Servicio)
+        {
+            switch (Servicio)
+            {
+                case "ejecutivo":
+                    return new Ejecutivo();
+                case "turista":
+                    return new Turista();
+                default:
+                    throw new ApplicationException(string.Format("Servicio '{0}' no puede ser brindado", Servicio));
+            }
+
+        }
+    }
+
+    /// la interface 'AbstractProductA' 
+
+    interface Hotel
+    {
+        string Nombre();
+    }
+
+
+    /// la interface 'AbstractProductB' 
+
+    interface Airplane
+    {
+        string Nombre();
+    }
+
+    /// <summary>
+    /// La clase 'ProductA1' class
+    /// </summary>
+    class Hotel3Estrellas : Hotel
+    {
+        public string Nombre()
+        {
+            return "Hotel 3 Estrellas";
+        }
+    }
+
+    /// <summary>
+    /// The 'ProductA2' class
+    /// </summary>
+    class Hotel5Estrellas : Hotel
+    {
+        public string Nombre()
+        {
+            return "Hotel5Estrellas";
+        }
+    }
+
+    /// <summary>
+    /// The 'ProductB1' class
+    /// </summary>
+    class Ejecutivo : Airplane
+    {
+        public string Nombre()
+        {
+            return "Asientos ejecutivos";
+        }
+    }
+
+    /// <summary>
+    /// The 'ProductB2' class
+    /// </summary>
+    class Turista : Airplane
+    {
+        public string Nombre()
+        {
+            return "Asientos Turista";
+        }
+    }
+
+    /// <summary>
+    /// The 'Client' class 
+    /// </summary>
+    class ProductoClient
+    {
+        Hotel objHotel;
+        Airplane objAirplane;
+
+        public ProductoClient(ProductoFactory factory, string tipo)
+        {
+            objHotel = factory.GetHotel(tipo);
+            objAirplane = factory.GetAirplane(tipo);
+        }
+
+        public string GetHotelNombre()
+        {
+            return objHotel.Nombre();
+        }
+
+        public string GetAirplaneNombre()
+        {
+            return objAirplane.Nombre();
+        }
+
+    }
+
+
+    /// Demostracion Patron Abstract Factory 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductoFactory hotel = new HotelFactory();
+            ProductoClient hotelclient = new ProductoClient(hotel, "3 estrellas");
+
+            Console.WriteLine("******* Hotel **********");
+            Console.WriteLine(hotelclient.GetHotelNombre());
+
+
+
+
+            ProductoFactory airplane = new AirplaneFactory();
+            ProductoClient airplaneclient = new ProductoClient(airplane, "turista");
+
+            Console.WriteLine("******* Airplane **********");
+
+            Console.WriteLine(airplaneclient.GetAirplaneNombre());
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
